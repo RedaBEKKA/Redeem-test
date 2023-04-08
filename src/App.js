525a77d545d19e8ce225edc40eaf6ef8984d94c0
RedaBEKKA <reda.bekka@gmail.com>
2023-04-08 18:49:44 +0100

    feat(Global):add helmet tags

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Helmet } from "react-helmet";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>Test for demo</p>
+      <Helmet>
+        <meta property="og:url" content="http://example.com/mypage" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Titre de ma page" />
+        <meta property="og:description" content="Description de ma page" />
+        <meta property="fb:app_id" content="votre_app_id_facebook" />
+      </Helmet>
+      <h1>Ma page</h1>
+      <p>Ceci est le contenu de ma page.</p>
     </div>
   );
 }
